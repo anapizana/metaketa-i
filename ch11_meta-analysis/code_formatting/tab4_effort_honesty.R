56e21f2aa10e62f6fc65773e089b8c4686859ade
clabima <cbm343@nyu.edu>
2019-04-01 02:00:46 -0400

    Correct scripts
    Corrects estimation function in `tab4_effort_honesty.R`
    Corrects typo in `tab8_context_hetero.R`

--- ch11_meta-analysis/code_formatting/tab4_effort_honesty.R
+++ ch11_meta-analysis/code_formatting/tab4_effort_honesty.R
@@ -1,7 +1,7 @@
 # Generate Table 11.4
 
 p_gb_m5_m24pool_unadj <- joint_ps(m5_m24c1_g, m5_m24c1_b) %>% round(., 3)
-p_gb_m6_m24pool_unadj <- joint_ps(m6_m24c1_g, m6_m24c1_g) %>% round(., 3)
+p_gb_m6_m24pool_unadj <- joint_ps(m6_m24c1_g, m6_m24c1_b) %>% round(., 3)
 
 # Round RI p-values
 ps <- sapply(list(m5_m24c1_g, m5_m24c1_b, m6_m24c1_g, m6_m24c1_b), function(l) round(l$p, 3))
@@ -36,7 +36,7 @@ fileConn <- file("tables/tab_11.4_effort_honesty_credibility.tex")
 n <- length(t11.4)
 ri_line <- grep("RI", t11.4)
 joint_p <- paste0("Joint RI $p$-value & \\multicolumn{2}{c}{", p_gb_m5_m24pool_unadj , "} & \\multicolumn{2}{c}{", p_gb_m6_m24pool_unadj , "} \\\\")
-notes <- "\\begin{flushleft}\\textit{Note:} The table reports the effects of information and the credibility of the information source on voter's perception of how hard-working (MPAP measure M5) and dishonest (MPAP measure M6) the incumbent politician is. We pool Benin, Burkina Faso, Uganda 1, and Uganda 2 in columns (1) and (3), and Benin, Burkina Faso, Mexico, and Uganda 2 in columns (2) and (4). Regressions include randomization block fixed effects; standard errors are clustered at the level of treatment assignment. $^*$ $p<0.05$; $^{**}$ $p<0.01$; $^{***}$ $p<0.001$ \\end{flushleft}"
+notes <- "\\begin{flushleft}\\textit{Note:} The table reports the effects of information and the credibility of the information source on voter's perception of how hard-working (MPAP measure M5) and dishonest (MPAP measure M6) the incumbent politician is. We pool Benin, Burkina Faso, Uganda 1, and Uganda 2 in columns (1) and (2), and Benin, Burkina Faso, Mexico, and Uganda 2 in columns (3) and (4). Regressions include randomization block fixed effects; standard errors are clustered at the level of treatment assignment. $^*$ $p<0.05$; $^{**}$ $p<0.01$; $^{***}$ $p<0.001$ \\end{flushleft}"
 header <- list("& \\multicolumn{2}{c}{Effort}&\\multicolumn{2}{c}{Dishonesty}\\\\",
                "\\cline{2-5}")
 t11.4 <- list(t11.4[1:9], header, t11.4[10:ri_line], joint_p, t11.4[(ri_line+1):(n-1)], notes, t11.4[n])
